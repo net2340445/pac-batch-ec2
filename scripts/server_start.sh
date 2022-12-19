398fcb97280728c111e6d6e9fdd4c1897c73a02d
andre chow <net.2340445@gmail.com>
2022-12-19 17:56:18 +0900

    scripts/server_validate.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,6 +2,6 @@
 exec >> /var/log/BatchBuild.log 2>&1
 echo "********************************************************************************************************************"
 echo $(date -u) "Starting to invoke server_start.sh"
-chmod 777 /root/scripts/pac-batch-ec2-0.1.jar
+chmod 777 /root/scripts/pac-batch-ec2-version-replace.jar
 sleep 5s
 echo $(date -u) "Ending server_start.sh"
