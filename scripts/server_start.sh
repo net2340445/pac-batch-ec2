596eed4f56395ae1861e026210f7b31542dfa0af
andre chow <net.2340445@gmail.com>
2022-12-19 17:35:44 +0900

    chmod 755 /root/scripts/pac-batch-ec2-0.1.jar

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,5 +2,6 @@
 exec >> /var/log/BatchBuild.log 2>&1
 echo "********************************************************************************************************************"
 echo $(date -u) "Starting to invoke server_start.sh"
+chmod 755 /root/scripts/pac-batch-ec2-0.1.jar
 sleep 5s
 echo $(date -u) "Ending server_start.sh"
