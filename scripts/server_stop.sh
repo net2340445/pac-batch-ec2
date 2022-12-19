0cd74ed60b6e3a8eb05ba618d9d677d1f175f151
andre chow <net.2340445@gmail.com>
2022-12-19 17:39:25 +0900

    /root/scripts/pac-batch-ec2-0.1.jar

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -2,5 +2,6 @@
 exec >> /var/log/BatchBuild.log 2>&1
 echo "********************************************************************************************************************"
 echo $(date -u) "Starting to invoke server_stop.sh"
+rm -rf /root/scripts/pac-batch-ec2-0.1.jar
 sleep 5s
 echo $(date -u) "Ending server_stop.sh"
--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -2,5 +2,5 @@
 exec >> /var/log/BatchBuild.log 2>&1
 echo "********************************************************************************************************************"
 echo $(date -u) "Starting to invoke sercer_validate.sh"
-ls -lrta /root/scripts/*.jar
+ls -lrta /root/scripts/pac-batch-ec2-0.1.jar
 echo $(date -u) "Ending sercer_validate.sh"
